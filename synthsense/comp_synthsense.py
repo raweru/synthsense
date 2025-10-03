4f4f68f50839a8f04f0ae9d3562321f1d494eb98
raweru <dominik.dekleva@hotmail.com>
2025-10-03 15:34:52 +0200

    Initial commit

---
+++ synthsense/comp_synthsense.py
@@ -0,0 +1,79 @@
+"""Compute easy-of-synthesis scores with AiZynthFinder.
+
+For AiZynthFinder documentation, see:
+- https://github.com/MolecularAI/aizynthfinder
+
+This implementations calls external command.
+The external command, in turn, can call AiZynthFinder command-line interface,
+or run CLI in parallel on a cluster, or even call REST API for AiZynth.
+External commands are left out of this repository.
+
+Simplest external command, with AiZynthFinder installed,
+would be the following bash script::
+
+  #!/usr/bin/env bash
+
+  cp /dev/stdin > input.smi && \
+  aizynthcli \
+     --smiles input.smi \
+     --config config.yml \
+     --output output.json.gz \
+     >>aizynth-stdout-stderr.txt 2>&1 && \
+  zcat output.json.gz
+
+"""
+
+__all__ = ["synthsense"]
+
+import logging
+import dataclasses
+
+from reinvent_plugins.normalize import normalize_smiles
+from reinvent_plugins.components.synthsense.parameters import Parameters, split_params
+
+from reinvent_plugins.components.synthsense.runner import run_aizynth
+
+from ..add_tag import add_tag
+from ..component_results import ComponentResults
+
+logger = logging.getLogger("reinvent")
+
+
+@add_tag("__component")
+class synthsense:
+    def __init__(self, params: Parameters):
+        self.no_cache = True  # Skip scoring cache.
+        self.params = params
+        self.smiles_type = "rdkit_smiles"  # For the normalizer.
+        logger.info("Initializing synthsense")
+            
+        self.params_run, self.endpoints = split_params(params)
+        self.steps = 0
+        self.number_of_endpoints = len(self.endpoints)
+
+        logger.info(f"synthsense params: {params}, run params: {self.params_run}")
+
+    @normalize_smiles
+    def __call__(self, smilies: list[str]) -> ComponentResults:
+        """Returns AiZynth score.
+
+        This function assumes it will start one synthsense run,
+        but we can extract multiple endpoints from one and the same run.
+        """
+
+        self.steps += 1
+        out = run_aizynth(smilies, self.params_run, self.steps)
+
+        # Here we can iterate over endpoints.
+        # Component results are returned as a list of numpy arrays.
+        # Each numpy array should be the size of `smilies`,
+        # i.e. it should contain all scores for one endpoint.
+        # Multiple endpoints go into the list as separate numpy arrays.
+        all_scores = []
+        for endpoint in self.endpoints:
+            scores = endpoint.get_scores(smilies, out)
+            all_scores.append(scores)
+
+        return ComponentResults(
+            scores=all_scores,
+        )
